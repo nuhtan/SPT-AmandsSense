1aec1253f1bc7142cc8416cfc09ef25db13d23dc
Nathaniel Richardson <31627786+nuhtan@users.noreply.github.com>
2026-09-14 14:02:56 -0400

    Clear doesn't work, set timer to clear time
    Update CooldownTime assignment logic in AmandsSenseClass.

--- AmandsSense/Components/AmandsSenseClass.cs
+++ AmandsSense/Components/AmandsSenseClass.cs
@@ -140,7 +140,7 @@ namespace AmandsSense.Components
                                 AlwaysOnToggle = !AlwaysOnToggle;
                             }
                             if (!AlwaysOnToggle) {
-                                Clear()
+                                CooldownTime = Settings.Cooldown.Value
                             }
                             CooldownTime = 0f;
                             CurrentOverlapLocation = 0;
