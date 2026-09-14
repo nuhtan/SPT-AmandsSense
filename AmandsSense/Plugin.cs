e1b699f033eb74994faf2e1b1339be4db6a26e6f
Nathaniel Richardson <31627786+nuhtan@users.noreply.github.com>
2026-09-14 13:25:39 -0400

    Change plugin identifier from 'xyz.drakia' to 'com.nuhtan'

--- AmandsSense/Plugin.cs
+++ AmandsSense/Plugin.cs
@@ -7,7 +7,7 @@ using AmandsSense.Patches;
 
 namespace AmandsSense
 {
-    [BepInPlugin("xyz.drakia.Sense", "Sense", "3.1.0")]
+    [BepInPlugin("com.nuhtan.Sense", "Sense", "3.1.0")]
     public class AmandsSensePlugin : BaseUnityPlugin
     {
         public static string PluginFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
